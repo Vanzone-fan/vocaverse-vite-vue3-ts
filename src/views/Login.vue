1faca2999248910d26e79dc421200488b1c8b29d
FanZong <fanzong889@gmail.com>
2024-08-14 11:18:04 +0800

    不通用封装 修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,11 @@
 <template>
 	<div class="bg-gradient-to-r from-sky-300 to-indigo-500">
 		<div class="flex items-center justify-center min-h-screen">
-			<AuthForm
-				:title="'Login'"
-				:buttonLabel="'Login'"
-				:successMessage="`${usernameValue}，欢迎回来！`"
-			/>
+			<LoginForm />
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-	import AuthForm from '../components/AuthForm.vue';
+	import LoginForm from '../components/LoginForm.vue';
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,11 @@
 <template>
 	<div class="bg-gradient-to-r from-sky-300 to-indigo-500">
 		<div class="flex items-center justify-center min-h-screen">
-			<AuthForm
-				:isRegister="true"
-				:title="'Register'"
-				:buttonLabel="'Register'"
-				:successMessage="`${usernameValue}，欢迎加入我们！`"
-				:errorMessage="'两次密码输入不一致，请重试。'"
-			/>
+			<RegisterForm />
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-	import AuthForm from '../components/AuthForm.vue';
+	import RegisterForm from '../components/RegisterForm.vue';
 </script>
